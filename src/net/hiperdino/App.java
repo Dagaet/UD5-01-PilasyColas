f5eb445f02faae163ac88c2d4796c6483a4e01ea
Saúl Arteaga <alojera15@gmail.com>
2023-05-01 20:40:30 +0100

    feat(Customer): setCustomerProducts & toString methods

--- src/net/hiperdino/App.java
+++ src/net/hiperdino/App.java
@@ -1,6 +1,11 @@
 package net.hiperdino;
+
+import net.hiperdino.customer.Customer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
+        Customer customer = new Customer();
+        customer.setCustomerProducts();
+        System.out.println(customer.toString());
     }
 }
--- src/net/hiperdino/customer/Customer.java
+++ src/net/hiperdino/customer/Customer.java
@@ -1,5 +1,37 @@
 package net.hiperdino.customer;
 
+import java.util.Scanner;
+import java.util.Stack;
+
+import net.hiperdino.utils.Utils;
+
 public class Customer {
     
+    private static final Scanner keyboard = new Scanner(System.in);
+    String name;
+    private Stack<Integer> customerProducts = new Stack<>();
+
+    public void setCustomerProducts() {
+        System.out.println("How many products do you carry?");
+        int amountOfProducts = keyboard.nextInt();
+        keyboard.nextLine();
+        for (int i = 0; i < amountOfProducts; i++) {
+            customerProducts.push(Utils.getRandomProduct());
+        }
+    }
+
+    @Override
+    public String toString(){
+        String msg = "==========================\n";
+        msg+= "Nombre: " + this.name;
+        msg+= "\n Total de productos: " + customerProducts.size();
+        msg+="\n Lista de articulos en la cesta: \n";
+        for (int i = 0; i < customerProducts.size(); i++) {
+            msg+= "-> " + customerProducts.get(i);
+            msg+= "\n";
+        }
+        msg+="\n==========================";
+        return msg;
+    }
+
 }
