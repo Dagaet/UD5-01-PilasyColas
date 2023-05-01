e2961ad858056f00496e80774e149a53fe0e2eeb
Saúl Arteaga <alojera15@gmail.com>
2023-05-01 19:56:41 +0100

    feat(Utils): finished random methods

--- src/net/hiperdino/utils/Utils.java
+++ src/net/hiperdino/utils/Utils.java
@@ -8,6 +8,11 @@ public class Utils {
         5, 7, 69, 8, 13, 72, 189, 233, 474, 522, 301, 600
     };
 
+    private static final String[] customerNames = new String[] {
+        "Amanda", "Sofía", "Flory", "Ana", "Flora", "Juan", "Pablo", "Jose Luis", "Valentin", "Ioan", 
+        "Esperanza", "Rita", "Eutimio", "Plinio", "Paloma", "Josué", "Damián", "Timoteo", "Jordi", "Carlos"
+    };
+
     public static Integer getRandomProduct() {
         Random random = new Random();
         int randomIndex = random.nextInt(Utils.products.length);
@@ -15,4 +20,10 @@ public class Utils {
         return Utils.products[randomIndex];
     }
 
+    public static String getRandomName() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(Utils.customerNames.length);
+        
+        return Utils.customerNames[randomIndex];
+    }
 }
